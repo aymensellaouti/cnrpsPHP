9813f51a396326415e2e8ccb9b080baa8258e01f
aymen <aymen.sellaouti@gmail.com>
2020-02-19 11:33:26 +0100

    PDO + Repoitory + update autoload.php

--- classesEtObjets/autoload.php
+++ classesEtObjets/autoload.php
@@ -1,7 +1,16 @@
 <?php
 
 function loadClass($nomClass) {
-    include $nomClass.'.php';
+    $file = $nomClass.'.php';
+    if( file_exists($file)) {
+        include $file;
+    } else {
+        $file = 'ExempleBD/'.$file;
+        if (file_exists($file)) {
+            include $file;
+        }
+    }
+
 }
 
 spl_autoload_register('loadClass');
